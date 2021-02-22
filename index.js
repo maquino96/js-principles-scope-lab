dd2e13f20eaeb0cc8b5e61393751f8f80a2f4c5b
maquino96 <matthewaquino96@gmail.com>
2021-02-21 23:20:55 -0500

    commit tree 4454521294ff2a0628c5f4d917f1b5a2cf2463e8

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName(){ 
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'whitney'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob, sadly :('
+
+}
+
